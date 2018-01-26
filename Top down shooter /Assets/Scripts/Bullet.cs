d41a643b6e5fe9ce0dafe420183907d6d9499c9d
xinoHITO <hitoshi_kamisato@hotmail.com>
2018-01-25 22:03:33 -0500

    modulo 3 clase 5 25.01
    balas te bajan vida
    granada que genera una explosion
    disparo de escopeta
    enemigo que recibe daño

--- Top down shooter /Assets/Scripts/Bullet.cs
+++ Top down shooter /Assets/Scripts/Bullet.cs
@@ -5,9 +5,15 @@ using UnityEngine;
 public class Bullet : MonoBehaviour {
 	public float force = 500;
 	public float deathTimer = 3;
+	public float shootRate = 0.1f;
+	public float damage = 20;
+
+	public bool destroyOnTrigger = true;
 	// Use this for initialization
 	void Start () {
-		GetComponent<Rigidbody2D> ().AddForce (transform.up * force);
+		if (force > 0) {
+			GetComponent<Rigidbody2D> ().AddForce (transform.up * force);	
+		}
 		Invoke ("DestroyBullet", deathTimer);
 
 
@@ -15,5 +21,14 @@ public class Bullet : MonoBehaviour {
 	
 	void DestroyBullet () {
 		Destroy (gameObject);
+	}
+
+	void OnTriggerEnter2D(Collider2D other){
+		if (other.tag == "Enemy") {
+			other.GetComponent<Health> ()._currentHealth -= damage;
+			if (destroyOnTrigger) {
+				Destroy (gameObject);	
+			}
+		}
 	}
 }
